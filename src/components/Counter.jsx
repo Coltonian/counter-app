ce3812abf7a1b677d0e918f002a92e37bb038491
Colton Neves <colton@simcase.io>
2018-10-29 11:46:49 -0400

    changed app to be a dice rolling app

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,37 +3,48 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         value: this.props.value,
-        tags: [],
+        diceRoll: this.props.diceRoll,
+        rollValue: 0,
+        numberOfDice: [],
+        // tags: [],
     };
     
-    renderTags() {
-        if(this.state.tags.length === 0) return <p>"There are no tags!"</p>;
-        return <ul> {this.state.tags.map(tag => <li key={tag}> { tag } </li>)} </ul>;
-    }
+   // renderTags() {
+   //    return <ul> {this.state.tags.map(tag => <li key={tag}> { tag } </li>)} </ul>;
+   //  }
 
     handleIncrement = () => {
-        this.setState({ value: this.state.value + 1 });
+        this.setState({ numberOfDice: [...this.state.numberOfDice, 1]});
     }
 
     handleDecrement = () => {
-        this.setState({ value: this.state.value - 1 });
+        let numberOfDice = [...this.state.numberOfDice];
+        numberOfDice.pop();
+        this.setState({ numberOfDice: [...numberOfDice]});
+    }
+
+    handleRoll = () => {
+        let rollValue = Math.floor(Math.random() * this.props.diceRoll.length);
+        this.setState({ rollValue: this.state.diceRoll[rollValue]});
     }
 
     render() { 
         return (
             <div>
                 {/* {this.props.children} */}
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button 
-                    onClick={this.handleIncrement} 
-                    className="btn btn-secondary btn-sm m-2"
-                >
-                    Increment
-                </button>
-                <button onClick={this.handleDecrement} className="btn btn-secondary btn-sm m-2">Decrement</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm m-2">(+)</button>
+                <button onClick={this.handleDecrement} className="btn btn-secondary btn-sm m-2">(-)</button>
+                <button onClick={this.handleRoll} className="btn btn-secondary btn-sm m-2">Roll Dice</button>
+                <button className="btn btn-secondary btn-sm m-2">{this.props.type}</button>
+                
+                {this.state.numberOfDice.map((value, index) => {
+                   return <span key={index} className={this.getBadgeClasses()}>{this.state.rollValue}</span>
+                })}
+                
             </div>
         );
     }
+
 
     formatCount() {
         let count = this.state.value;
--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 import Counter from './Counter';
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            { id: 1, value: 4},
-            { id: 2, value: 3},
-            { id: 3, value: 2},
-            { id: 4, value: 0},
-        ],
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            counters: [
+                { id: 1, value: 4, type: "d4", diceRoll: [1, 2, 3, 4]},
+                { id: 2, value: 3, type: "d8", diceRoll: [1, 2, 3, 4, 5, 6]},
+                { id: 3, value: 2, type: "d10", diceRoll: [1, 2, 3, 4, 5, 6, 7, 8]},
+                { id: 4, value: 0, type: "d12", diceRoll: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]},
+            ]
+        };
+    }
+
 
     render() { 
         return (
             <div>
                 {this.state.counters.map(counter => 
-                    <Counter key={counter.id} value={counter.value}>
+                    <Counter key={counter.id} value={counter.value} type={counter.type} diceRoll={counter.diceRoll}>
                         {/* <h4>Counter #{counter.id}</h4> */}
                     </Counter>
                 )}
